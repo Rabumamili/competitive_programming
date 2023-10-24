5513a63f4c9337cc8410e518228e0dc9cbf5a387
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2023-10-24 14:01:23 +0300

    Create #438. Find All Anagrams in a String.py

---
+++ #438. Find All Anagrams in a String.py
@@ -0,0 +1,20 @@
+class Solution(object):
+     
+    def findAnagrams(self, s, p):
+        result = []
+        p_freq = [0] * 26
+        window_freq = [0] * 26
+
+        for char in p:
+            p_freq[ord(char) - ord('a')] += 1
+
+        for i in range(len(s)):
+            window_freq[ord(s[i]) - ord('a')] += 1
+
+            if i >= len(p):
+                window_freq[ord(s[i - len(p)]) - ord('a')] -= 1
+
+            if window_freq == p_freq:
+                result.append(i - len(p) + 1)
+
+        return result
