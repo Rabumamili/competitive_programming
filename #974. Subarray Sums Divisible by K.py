aec31e6907aac325c3a360422ec2434620e38fce
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2023-10-26 15:34:38 +0300

    Create #974. Subarray Sums Divisible by K.py
    https://leetcode.com/problems/subarray-sums-divisible-by-k/

---
+++ #974. Subarray Sums Divisible by K.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def subarraysDivByK(self, nums, k):
+        prefix_sums = {0: 1}
+        count = 0
+        prefix_sum = 0
+
+        for num in nums:
+            prefix_sum += num
+            mod = prefix_sum % k
+            if mod < 0:
+                mod += k
+            if mod in prefix_sums:
+                count += prefix_sums[mod]
+            prefix_sums[mod] = prefix_sums.get(mod, 0) + 1
+
+        return count
+         
+        
