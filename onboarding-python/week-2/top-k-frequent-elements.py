40db7af944696dd200bf9661a9b2ffa7d79603b3
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2023-12-17 20:40:51 +0300

    Added solution for Top K Frequent Elements

---
+++ onboarding-python/week-2/top-k-frequent-elements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count, unique, c = [], [], []
+
+        for i in nums:
+            if i not in unique:
+                unique.append(i)
+                c.append(nums.count(i))
+
+        for i in range(k):
+            m=max(c)
+            i=c.index(m)
+            count.append(unique[i])
+            c[i]=-1
+        return count
