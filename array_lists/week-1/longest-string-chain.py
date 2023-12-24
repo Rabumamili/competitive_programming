8cad7acd576ebde9b55d4cae304bf27ae95f6dc0
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2023-12-24 22:31:42 +0300

    Added solution for Longest String Chain

---
+++ array_lists/week-1/longest-string-chain.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        words.sort(key=len)
+        dp = {}
+        
+        for word in words:
+            dp[word] = 1
+            
+            # Find all possible predecessors of the current word
+            for i in range(len(word)):
+                predecessor = word[:i] + word[i+1:]
+                
+                if predecessor in dp:
+                    dp[word] = max(dp[word], dp[predecessor] + 1)
+        
+        return max(dp.values())
