73f0c0d2417ddbc8537ca77de7a001e51ce6f246
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2023-12-26 19:22:37 +0300

    Added solution for Sort Colors

---
+++ array_lists/week-1/sort-colors.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    
+    def sortColors(self, nums):
+
+        start = 0
+        mid = 0
+        end = len(nums) - 1
+
+        while mid <= end:
+
+            if nums[mid] == 0:
+                # Swap with start index
+                self.swap(nums, start, mid)
+                mid += 1
+                start += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            elif nums[mid] == 2:
+                # Swap with end index
+                self.swap(nums, mid, end)
+                end -= 1
+
+    def swap(self, arr, pos1, pos2):
+        temp = arr[pos1]
+        arr[pos1] = arr[pos2]
+        arr[pos2] = temp
