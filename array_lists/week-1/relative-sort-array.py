fcc15c4eafa5fc66ce848d92cd9e728155c7b20b
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2023-12-27 19:24:15 +0300

    Added solution for Relative Sort Array

--- array_lists/week-1/relative-sort-array.py
+++ array_lists/week-1/relative-sort-array.py
@@ -6,20 +6,30 @@ class Solution(object):
         # :rtype: List[int]
         # """
         
-        ans =[]
-        remaining =[]
-        for nums in arr2:
-            count = arr1.count(nums)
-            arr = [nums]*count
-            ans.extend(arr)
+        # ans =[]
+        # remaining =[]
+        # for nums in arr2:
+        #     count = arr1.count(nums)
+        #     arr = [nums]*count
+        #     ans.extend(arr)
         
-        for i in range(len(arr1)):
-            if arr1[i] not in ans:
-                remaining.append(arr1[i])
-        remaining.sort()
-        ans.extend(remaining)
+        # for i in range(len(arr1)):
+        #     if arr1[i] not in ans:
+        #         remaining.append(arr1[i])
+        # remaining.sort()
+        # ans.extend(remaining)
         
-        return ans
+        # return ans
+         
+        #   using comparator 
+        def comparator(num):
+            if num in arr2:
+                return arr2.index(num)
+            else:
+                return len(arr2) + num
+
+        return sorted(arr1, key=comparator)
+
 
         
      
