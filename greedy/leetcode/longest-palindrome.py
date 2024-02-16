bea8e138d0c369067972e520dc1d780b091e10a7
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-02-16 15:37:52 +0300

    Add solution for Longest Palindrome

---
+++ greedy/leetcode/longest-palindrome.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        char_count={}
+        for char in s:
+            char_count[char]= char_count.get(char,0) + 1
+            
+        final= 0
+        odd_found= False
+
+        for char in char_count:
+            if char_count[char]%2 == 0:
+                final +=char_count[char]
+            else:
+                final += (char_count[char] -1)
+                odd_found = True
+        if odd_found: 
+            final += 1
+
+        return final
+
