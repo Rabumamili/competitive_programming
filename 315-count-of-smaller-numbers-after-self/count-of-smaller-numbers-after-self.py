6f04679b3645295ce7b91d44ed1833e8060dbf4c
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-03-22 10:25:08 +0300

    Time: 1232 ms (94.26%) | Memory: 37 MB (59.82%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
@@ -1,23 +1,32 @@
+from sortedcontainers import SortedList
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        nums = nums[::-1]
-        nums = [(n,i) for i,n in enumerate(nums)]
-        res = [0]*len(nums)
-        
-        def mergesort(left,right):
-            if left == right:
-                return 
-            mid = (left+right)//2
-            mergesort(left,mid)
-            mergesort(mid+1,right)
-            
-            i = left
-            for j in range(mid+1,right+1):
-                while i < mid+1 and nums[i][0] < nums[j][0]:
-                    i += 1
-                res[nums[j][1]] += i-left
-       
-            nums[left:right+1] = sorted(nums[left:right+1])
-            
-        mergesort(0,len(nums)-1) if nums else None
-        return res[::-1]
+        sortedarr= SortedList()
+        ans = []
+        for num in nums[::-1]:
+            idx = sortedarr.bisect_left(num)
+            ans.append(idx)
+            sortedarr.add(num)
+        return ans[::-1]
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+        # s = SortedList()
+        # output = []
+        # for n in nums[::-1]:
+        #     ans = SortedList.bisect_left(s, n)
+        #     output.append(ans)
+        #     SortedList.add(n)
+        # return output[::-1]
