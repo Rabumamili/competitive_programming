3e9c521cabd382cbb83303dbfc6d9b692aabed68
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-03-26 15:15:34 +0300

    Time: 5093 ms (6.66%) | Memory: 17.9 MB (93.89%) - LeetSync

--- 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.py
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.py
@@ -3,20 +3,21 @@ class Solution:
         n = 1
         for num in nums:
             n *= num
-            
+
         def primefactors(n):
             factors = []
             divisor = 2
     
             while n > 1:
                  
-                while n % divisor == 0:
+                if n % divisor == 0:
                     if len(factors)==0 or factors[-1] != divisor:
 
                         factors.append(divisor)
                     n //= divisor
                  
-                divisor += 1
+                else:
+                    divisor += 1
             
             return len(factors)
         return primefactors(n)
