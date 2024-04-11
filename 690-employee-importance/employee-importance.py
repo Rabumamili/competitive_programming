34dccf3af00a5d5b4f71f3a60708c5a48243880a
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-04-11 14:00:59 +0300

    Time: 119 ms (38.32%) | Memory: 17.9 MB (48.45%) - LeetSync

--- 690-employee-importance/employee-importance.py
+++ 690-employee-importance/employee-importance.py
@@ -8,19 +8,22 @@ class Employee:
 """
 
 class Solution:
-    res = 0
+    
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        hmap = collections.defaultdict(int)
-        # create map if emp=> idx
-        for i, emp in enumerate(employees):
-            hmap[emp.id] = i
-
-        self.dfs(employees, hmap[id], hmap)
-        return self.res
-
-    def dfs(self, employees, id, hmap):        
-        self.res += employees[id].importance
-        for sub_ord in employees[id].subordinates:
-            self.dfs(employees,hmap[sub_ord], hmap)
+        if employees == []:
+            return 0
         
-        return self.res
+        ans = 0
+        hmap = {}
+        for employee in employees:
+            hmap[employee.id] = employee
+        
+        queue = deque()
+        queue.append(id)
+        while queue:
+            employee = hmap[queue.popleft()]
+            ans += employee.importance
+
+            for subs in employee.subordinates:
+                queue.append(subs)
+        return ans
