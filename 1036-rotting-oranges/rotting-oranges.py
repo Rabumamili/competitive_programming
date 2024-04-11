91e6cc8174b9a4cff0bc1ecbbd6ed6ae032bf660
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-04-11 17:52:49 +0300

    Time: 49 ms (65.64%) | Memory: 16.5 MB (95.73%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,37 +1,35 @@
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        oranges_count = 0
-        time = -1
+        fresh = 0
+        time = 0
         q = deque()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    oranges_count += 1
+                    fresh += 1
                 elif grid[i][j] == 2:
                     q.append((i, j))
 
-        if oranges_count == 0:
+        if fresh == 0:
             return 0
 
-        dirs = [(1,0), (0,1), (-1,0), (0,-1)]
-        
-        while len(q) > 0:
-            print(len(q))
+        direction = [(1,0), (0,1), (-1,0), (0,-1)]
+        while len(q) > 0 and fresh > 0:
             for _ in range(len(q)):
                 node = q.popleft()
-                for i in range(len(dirs)):
+
+                for i in range(len(direction)):
                     row, col = node
-                    row += dirs[i][0]
-                    col += dirs[i][1]
+                    row += direction[i][0]
+                    col += direction[i][1]
+
                     if row >= 0 and row < len(grid) and col >= 0 and col < len(grid[0]) and grid[row][col] == 1:
                         q.append((row, col))
-                        oranges_count -= 1
+                        fresh -= 1
                         grid[row][col] = 2
+
             time += 1
         
-        if oranges_count == 0:
-            return time
-        else:
-            return -1
+        return time if fresh == 0 else -1
             
