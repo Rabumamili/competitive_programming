c75b98b7c8702aadb1abda2f82990d29146564af
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-04-24 22:28:24 +0300

    Time: 34 ms (59.23%) | Memory: 16.5 MB (31.32%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        nums = [0, 1,1]
+        for i in range(3, 38):
+            nums.append((nums[i-1]+ nums[i-2]+nums[i-3]))
+        return nums[n]
