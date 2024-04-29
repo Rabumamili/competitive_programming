899ae6dc429e1f83088654d1a7c36671b3401e9c
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-04-29 22:55:13 +0300

    Time: 154 ms (71.53%) | Memory: 17.2 MB (51.55%) - LeetSync

---
+++ 529-minesweeper/minesweeper.py
@@ -0,0 +1,34 @@
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        def dfs(board, i, j):
+            if board[i][j] == 'M':
+                board[i][j] = 'X'  # If mine is clicked, game over
+                return
+            if board[i][j] != 'E':
+                return
+ 
+            count = 0
+            for dx in range(-1, 2):
+                for dy in range(-1, 2):
+                    if dx == 0 and dy == 0:
+                        continue
+                    x, y = i + dx, j + dy
+                    if 0 <= x < m and 0 <= y < n and board[x][y] == 'M':
+                        count += 1
+
+            if count > 0:
+                board[i][j] = str(count)   
+            else:
+                board[i][j] = 'B'   
+                for dx in range(-1, 2):
+                    for dy in range(-1, 2):
+                        if dx == 0 and dy == 0:
+                            continue
+                        x, y = i + dx, j + dy
+                        if 0 <= x < m and 0 <= y < n:
+                            dfs(board, x, y)
+
+        m, n = len(board), len(board[0])
+        click_r, click_c = click
+        dfs(board, click_r, click_c)
+        return board
