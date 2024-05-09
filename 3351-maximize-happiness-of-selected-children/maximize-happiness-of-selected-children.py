971260fd73b8c53f59ceb6687db688b2ab6c3acc
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-05-09 23:03:42 +0300

    Time: 798 ms (72.78%) | Memory: 43.5 MB (34.74%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -4,11 +4,9 @@ class Solution:
         ans = happiness[0]
         i = 1
         while k > 1 and i < len(happiness):
-            print(ans)
+            
             if happiness[i]-i > 0:
                 ans += happiness[i]-i
-            print( happiness[i]-1)
-            print(ans)
             k -= 1
             i += 1
         return ans
