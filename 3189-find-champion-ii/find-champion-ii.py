95f2999a91b7f9eec219cc25cce28ba66cba84bb
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-05-14 23:01:07 +0300

    Time: 743 ms (91.96%) | Memory: 18.1 MB (18.48%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.py
+++ 3189-find-champion-ii/find-champion-ii.py
@@ -1,16 +1,11 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
         in_degree = [0] * n
-    
-        
         for edge in edges:
             k, to_team = edge
             in_degree[to_team] += 1
-        
-    
+
         champions = [i for i, degree in enumerate(in_degree) if degree == 0]
-        
-         
         if len(champions) == 1:
             return champions[0]
         else:
