4e7c36c958a7835f56dbc2e2e748a0b5b00b94d5
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-05-15 11:04:17 +0300

    Time: 32 ms (83.96%) | Memory: 16.6 MB (43.96%) - LeetSync

--- 343-integer-break/integer-break.py
+++ 343-integer-break/integer-break.py
@@ -1,18 +1,11 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-        memo = {}
-        def dp(n):
-            if n in memo:
-                return memo[n]
-            
-            if n == 2 or n == 3:
-                return n-1
-            
-            ans = 1
-            while n > 4:
-                n -= 3
-                ans *=3                
-            memo[n] = ans*n
-            return memo[n]
+
+        if n == 2 or n == 3:
+            return n-1
         
-        return dp(n)
+        ans = 1
+        while n > 4:
+            n -= 3
+            ans *=3                
+        return ans*n
