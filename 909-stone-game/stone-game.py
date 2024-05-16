768d2195b5c812460ebc6a10fde2ceff7d698ae7
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-05-16 10:15:11 +0300

    Time: 400 ms (22.86%) | Memory: 127.7 MB (13.12%) - LeetSync

---
+++ 909-stone-game/stone-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        memo = {}
+        n = len(piles)
+        
+        def dp(i, j):
+            if i == j:
+                return piles[i]
+            if (i, j) not  in memo:
+                memo[(i, j)] = max(piles[i] + dp(i+1, j), piles[j] + dp(i, j-1))
+            return memo[(i, j)]
+        
+        return sum(piles) - 2*dp(0, n-1) < 0
+        
+             
