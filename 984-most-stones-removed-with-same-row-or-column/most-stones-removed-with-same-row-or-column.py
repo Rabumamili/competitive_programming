6609648d6182a27b398af5c3eaddcd26d0da4c9f
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-05-28 20:51:03 +0300

    Time: 662 ms (28.75%) | Memory: 17.9 MB (27.55%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -1,42 +1,21 @@
 class Solution:
-    class UnionFind:
-        def __init__(self):
-            self.parent = {}
-            self.rank = {}
-        
-        def find(self, x):
-            if self.parent[x] != x:
-                self.parent[x] = self.find(self.parent[x])
-            return self.parent[x]
-        
-        def union(self, x, y):
-            rootX = self.find(x)
-            rootY = self.find(y)
-            
-            if rootX != rootY:
-                if self.rank[rootX] > self.rank[rootY]:
-                    self.parent[rootY] = rootX
-                elif self.rank[rootX] < self.rank[rootY]:
-                    self.parent[rootX] = rootY
-                else:
-                    self.parent[rootY] = rootX
-                    self.rank[rootX] += 1
-        
-        def add(self, x):
-            if x not in self.parent:
-                self.parent[x] = x
-            self.rank[x] = 0
     def removeStones(self, stones: List[List[int]]) -> int:
-        nums = [[0,0],[2,2],[10000,2]]
-        if stones == nums:
-            return 1
-        uf = self.UnionFind()
-        
-        for x, y in stones:
-            uf.add(x)
-            uf.add(y + 10000)   
-            uf.union(x, y + 10000)
-        
-        unique_groups = len({uf.find(x) for x, y in stones} | {uf.find(y + 10000) for x, y in stones})
-        return len(stones) - unique_groups
-    
+        def dfs(node):
+            nonlocal moves
+            visited.add(node)
+            for next_node in graph[node]:
+                if next_node not in visited:
+                    moves += 1
+                    dfs(next_node)
+
+                    
+        graph , moves , visited = defaultdict(list) , 0 , set()
+        for i in range(len(stones)):
+            for j in range(i):
+                if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
+                    graph[i].append(j)
+                    graph[j].append(i)
+        for node in range(len(stones)):
+            if node not in visited:
+                dfs(node)
+        return moves
