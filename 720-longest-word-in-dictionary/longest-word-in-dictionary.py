2bcedc768d78ed52b31f202603f441d3345c4876
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-06-04 21:50:12 +0300

    Time: 102 ms (64.56%) | Memory: 17.7 MB (23.80%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.py
@@ -1,14 +1,38 @@
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        self.longest_word = ""
+    
+    def insert(self, word):
+        current = self.root
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        current.is_end_of_word = True
+
+    def dfs(self, node, path):
+        # Check if the current node forms a valid word
+        if len(path) > len(self.longest_word) or (len(path) == len(self.longest_word) and path < self.longest_word):
+            self.longest_word = path
+
+        for char, next_node in node.children.items():
+            if next_node.is_end_of_word:
+                self.dfs(next_node, path + char)
+
+ 
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        words.sort()   
-        buildable = set([""])   
-        longest = ""
-
-        for word in words:
-            
-            if word[:-1] in buildable:
-                buildable.add(word)
-                if len(word) > len(longest):
-                    longest = word
+        trie = Trie()
+        for word in sorted(words):
+            trie.insert(word)
+      
+        trie.dfs(trie.root, "")
         
-        return longest
+        return trie.longest_word
