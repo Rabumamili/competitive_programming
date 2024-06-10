828018da78bce4ecb028b04df5a5103bdd8740b1
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-06-10 05:54:33 +0300

    Time: 38 ms (90.09%) | Memory: 16.7 MB (56.80%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,18 +1,17 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        max_day = max(days) + 1
-        travel = set(days)
-        memo = {}
-        def dp(n):
-            if n <= 0:
-                return 0
-            if n in memo:
-                return memo[n]
-            if n not in travel:
-                memo[n] = dp(n-1)
+        max_day = max(days)
+        travel_days = set(days)
+        dp = [0] * (max_day + 1)
+        
+        for day in range(1, max_day + 1):
+            if day not in travel_days:
+                dp[day] = dp[day - 1]
             else:
-                memo[n] = min(dp(n-1) + costs[0], dp(n-7) + costs[1], dp(n-30)+ costs[2])
-            return memo[n]
-        return dp(max_day -1)
-            
- 
+                dp[day] = min(
+                    dp[day - 1] + costs[0], 
+                    dp[max(0, day - 7)] + costs[1], 
+                    dp[max(0, day - 30)] + costs[2]
+                )
+                
+        return dp[max_day]
