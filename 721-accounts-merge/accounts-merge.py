c467d333efbf68a809e25b92e71558397309be69
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-06-11 21:41:06 +0300

    Time: 180 ms (49.85%) | Memory: 22.1 MB (28.57%) - LeetSync

---
+++ 721-accounts-merge/accounts-merge.py
@@ -0,0 +1,32 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        email_to_name = {}
+        graph = defaultdict(set)
+        for acc in accounts:
+            name = acc[0]
+            first_email = acc[1]
+            for email in acc[1:]:
+                graph[first_email].add(email)
+                graph[email].add(first_email)
+                email_to_name[email] = name
+    
+        seen = set()
+        def dfs(email):
+            stack = [email]
+            component = []
+            while stack:
+                node = stack.pop()
+                if node not in seen:
+                    seen.add(node)
+                    component.append(node)
+                    stack.extend(graph[node])
+            return component
+
+        # Step 3: Collect the results
+        merged_accounts = []
+        for email in email_to_name:
+            if email not in seen:
+                component = dfs(email)
+                merged_accounts.append([email_to_name[email]] + sorted(component))
+
+        return merged_accounts
