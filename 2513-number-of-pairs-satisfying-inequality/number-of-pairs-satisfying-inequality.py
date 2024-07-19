23f40e5760fb7c31f13c126724240396760c9113
Rabumamili <145286734+Rabumamili@users.noreply.github.com>
2024-07-19 09:30:35 +0300

    Time: 793 ms (85.71%) | Memory: 35.4 MB (52.60%) - LeetSync

--- 2513-number-of-pairs-satisfying-inequality/number-of-pairs-satisfying-inequality.py
+++ 2513-number-of-pairs-satisfying-inequality/number-of-pairs-satisfying-inequality.py
@@ -2,9 +2,6 @@ from sortedcontainers import SortedList
 
 class Solution:
     def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
-        # nums1[i] - nums1[j] <= nums2[i] - nums2[j] + diff
-        # nums1[i] - nums2[i] <= nums1[j] - nums2[j] + diff
- 
         sorted_arr = SortedList()
         n = len(nums1)
         count = 0
