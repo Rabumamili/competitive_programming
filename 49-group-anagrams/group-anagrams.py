302e86c6dc18ef25910dcc1dd8c8a833ad9e7fa5
Rabuma Milisha Dugasa <145286734+Rabumamili@users.noreply.github.com>
2024-09-01 19:51:49 +0300

    Time: 82 ms (87.91%) | Memory: 19.6 MB (75.24%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,8 +1,7 @@
-class Solution(object):
-     
-    def groupAnagrams(self, strs ) :
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = {}
-
+        
         for word in strs:
             sorted_word = ''.join(sorted(word))
             if sorted_word not in anagrams:
