3ab8262da0e10fbd21296ece66c3032e36eaee88
Rabuma Milisha Dugasa <145286734+Rabumamili@users.noreply.github.com>
2024-10-18 19:34:46 +0300

    Time: 44 ms (100.00%) | Memory: 17.1 MB (99.76%) - LeetSync

--- 2187-vowels-of-all-substrings/vowels-of-all-substrings.py
+++ 2187-vowels-of-all-substrings/vowels-of-all-substrings.py
@@ -1,9 +1,10 @@
 class Solution:
     def countVowels(self, word: str) -> int:
         counts = 0
-        s = "aeiou"
         n = len(word)
+        s = "aeiou"
         for i in range(n):
             if word[i] in s:
-                counts  += (i+1)*(n-i)
+                counts += (i+1)*(n-i)
+                
         return counts
